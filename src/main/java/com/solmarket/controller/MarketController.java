0927242b0bebab4ce8bf61602da30aa822d16e6f
hayeong <hayeong0624@gmail.com>
2022-06-06 15:21:29 +0900

    MarketController 수정

--- src/main/java/com/solmarket/controller/MarketController.java
+++ src/main/java/com/solmarket/controller/MarketController.java
@@ -1,6 +1,5 @@
 package com.solmarket.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,11 +10,38 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 public class MarketController {
 	
-//	@Autowired
-//	private MarketService service;
+	@GetMapping("/market_accept")
+	public void market_accept() {
+		log.info("[GetMapping] ========== 참여 승인 리스트 호출 ==========");
+	}
 	
 	@GetMapping("/market_detail")
 	public void market_detail() {
 		log.info("[GetMapping] ========== 장터 상세 페이지 호출 ==========");
+	}
+	
+	@GetMapping("/market_myseller")
+	public void seller_list() {
+		log.info("[GetMapping] ========== 장터 참여자 리스트 호출 ==========");
+	}
+	
+	@GetMapping("/market_notice")
+	public void market_notice() {
+		log.info("[GetMapping] ========== 장터 공지 리스트 호출 ==========");
+	}
+	
+	@GetMapping("/market_receive")
+	public void market_receive() {
+		log.info("[GetMapping] ========== 장터 참여 신청 리스트 호출 ==========");
+	}
+	
+	@GetMapping("/market_register")
+	public void market_register() {
+		log.info("[GetMapping] ========== 장터 등록 폼 호출 ==========");
+	}
+	
+	@GetMapping("/market_review")
+	public void market_review() {
+		log.info("[GetMapping] ========== 장터 리뷰 리스트 호출 ==========");
 	}
 }
