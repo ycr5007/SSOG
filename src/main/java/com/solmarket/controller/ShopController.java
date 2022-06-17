1e0307c1adb83b340c9522d294b1c9c2bf90a998
seunghwan <ksh7353@naver.com>
2022-06-17 17:04:28 +0900

    order 페이지 변경

--- src/main/java/com/solmarket/controller/ShopController.java
+++ src/main/java/com/solmarket/controller/ShopController.java
@@ -1,6 +1,8 @@
 package com.solmarket.controller;
 
 
+import java.util.Random;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -82,19 +84,22 @@ public class ShopController {
 		UserDTO userDto = service.getOrderUser(userNo);
 		ProductDTO productDto = service.getOrderProduct(productNo);
 		log.info("" + productDto);
+		log.info("userDto 확인"+ userDto);
 		model.addAttribute("user", userDto);
 		model.addAttribute("product", productDto);
 		model.addAttribute("orderQN", orderQN);
 	}
 	
 	@PostMapping("/iamport")
-	public String payinfo(Model model,UserDTO userDto,OrderDTO orderDto) {
+	public String payinfo(Model model,UserDTO userDto,OrderDTO orderDto,ProductDTO productDto) {
 		log.info("payInfo");
 		log.info("userDto" + userDto);
 		log.info("orderDto" + orderDto);
+		log.info("productDto" + productDto);
 //		log.info("userName",userDto);
 		model.addAttribute("user",userDto);
 		model.addAttribute("order",orderDto);
+		model.addAttribute("product",productDto);
 		return "/shop/iamport";
 	}
 	
@@ -111,5 +116,36 @@ public class ShopController {
 //	 
 //	}
 //	
+	
+// ----------------------------------------------------------------
+	
+// 주문번호 생성 메소드 -> 연월일+랜덤4자리 -> 보류!
+//public static String numberGen(int len, int dupCd ) {
+//        
+//        Random rand = new Random();
+//        String numStr = ""; //난수가 저장될 변수
+//        
+//        for(int i=0;i<len;i++) {
+//            
+//            //0~9 까지 난수 생성
+//            String ran = Integer.toString(rand.nextInt(10));
+//            
+//            if(dupCd==1) {
+//                //중복 허용시 numStr에 append
+//                numStr += ran;
+//            }else if(dupCd==2) {
+//                //중복을 허용하지 않을시 중복된 값이 있는지 검사한다
+//                if(!numStr.contains(ran)) {
+//                    //중복된 값이 없으면 numStr에 append
+//                    numStr += ran;
+//                }else {
+//                    //생성된 난수가 중복되면 루틴을 다시 실행한다
+//                    i-=1;
+//                }
+//            }
+//        }
+//        return numStr;
+//    }
+
 
 }
